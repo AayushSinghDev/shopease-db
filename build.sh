6db11fc3452ee23e5a027cd29702581b0e5229c9
Aayush Singh <aks0001101@gmail.com>
2026-03-25 11:46:42 +0530

    Refactor build script to use import_data.py
    Removed superuser creation commands from build script and replaced with import_data.py execution.

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'adm..." | python manage.py shell
-python manage.py shell -c "
-from accounts.models import SuperAdmin
-if not SuperAdmin.objects.filter(email='admin@shopease.com').exists():
-    SuperAdmin.objects.create(name='admin', email='admin@shopease.com', password='pbkdf2_sha256$260000$kmO1bVhYkeMT$OBmNGuBm1KAItFjcqEU1QQq1AnyCBzxvG8lZElbVozo=')
-"
-psql $DATABASE_URL < shopease_postgres_import.sql
+python import_data.py
